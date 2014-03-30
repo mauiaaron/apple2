98cd97532832262c3e2183347f828f72df3ff48c
Aaron Culliney <asc_at_bitr0t_com>
2014-03-30 10:39:45 -0700

    ifndef MIN() & MAX() macros

--- src/common.h
+++ src/common.h
@@ -64,6 +64,14 @@
 #include "audio/mockingboard.h"
 #endif
 
+#if !defined(MIN)
+#define MIN(a,b) (((a) <= (b)) ? (a) : (b))
+#endif
+
+#if !defined(MAX)
+#define MAX(a,b) (((a) >= (b)) ? (a) : (b))
+#endif
+
 #ifndef NDEBUG
 #       if defined(__GNUC__)
 #               pragma GCC diagnostic push
