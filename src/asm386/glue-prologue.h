f3ea9fa2562eb29c8b70915a757e42544e8401ec
Aaron Culliney <asc_at_bitr0t_com>
2014-05-10 16:31:07 -0700

    tabs -> spaces

--- src/asm386/glue-prologue.h
+++ src/asm386/glue-prologue.h
@@ -18,13 +18,13 @@
 #include "apple2.h"
 #include "misc.h"
 
-#define GLUE_FIXED_READ(func,address)			\
-E(func)			movb	SN(address)(%edi),%al;  \
-			ret;
+#define GLUE_FIXED_READ(func,address) \
+E(func)                 movb    SN(address)(%edi),%al; \
+                        ret;
 
-#define GLUE_FIXED_WRITE(func,address)                  \
-E(func)			movb	%al,SN(address)(%edi);  \
-			ret;
+#define GLUE_FIXED_WRITE(func,address) \
+E(func)                 movb    %al,SN(address)(%edi); \
+                        ret;
 
 #define GLUE_BANK_MAYBEREAD(func,pointer) \
 E(func)                 testl   $SS_CXROM, SN(softswitches); \
@@ -37,52 +37,52 @@ E(func)                 testl   $SS_CXROM, SN(softswitches); \
                         ret;
 
 #define GLUE_BANK_READ(func,pointer) \
-E(func)			addl	SN(pointer),%edi;	\
-			movb	(%edi),%al;		\
-			subl	SN(pointer),%edi;	\
-			ret;
+E(func)                 addl    SN(pointer),%edi; \
+                        movb    (%edi),%al; \
+                        subl    SN(pointer),%edi; \
+                        ret;
 
 #define GLUE_BANK_WRITE(func,pointer) \
-E(func)			addl	SN(pointer),%edi;	\
-			movb	%al,(%edi);		\
-			subl	SN(pointer),%edi;	\
-			ret;
+E(func)                 addl    SN(pointer),%edi; \
+                        movb    %al,(%edi); \
+                        subl    SN(pointer),%edi; \
+                        ret;
 
 #define GLUE_BANK_MAYBEWRITE(func,pointer) \
-E(func)			addl	SN(pointer),%edi;	\
-			cmpl	$0,SN(pointer);		\
-			jz	1f;			\
-			movb	%al,(%edi);		\
-1:			ret;
+E(func)                 addl    SN(pointer),%edi; \
+                        cmpl    $0,SN(pointer); \
+                        jz      1f; \
+                        movb    %al,(%edi); \
+1:                      ret;
 
 
 // TODO FIXME : implement CDECL prologue/epilogues...
 #define GLUE_C_WRITE(func) \
-E(func)			pushl	%eax;			\
-			pushl	%ecx;			\
-			pushl	%edx;			\
-			andl	$0xff,%eax;		\
-			pushl	%eax;			\
-			pushl	%edi;			\
-			call	SN(c_##func);           \
-			popl	%edx; /* dummy */	\
-			popl	%edx; /* dummy */	\
-			popl	%edx;			\
-			popl	%ecx;			\
-			popl	%eax;			\
-			ret;
+E(func)                 pushl   %eax; \
+                        pushl   %ecx; \
+                        pushl   %edx; \
+                        andl    $0xff,%eax; \
+                        pushl   %eax; \
+                        pushl   %edi; \
+                        call    SN(c_##func); \
+                        popl    %edx; /* dummy */ \
+                        popl    %edx; /* dummy */ \
+                        popl    %edx; \
+                        popl    %ecx; \
+                        popl    %eax; \
+                        ret;
 
 // TODO FIXME : implement CDECL prologue/epilogues...
 #define GLUE_C_READ(func) \
-E(func)			/*pushl	%eax;*/			\
-			pushl	%ecx;			\
-			pushl	%edx;			\
-			pushl	%edi;			\
-			call	SN(c_##func);           \
-			/*movb	%al,12(%esp);*/		\
-			popl	%edx; /* dummy */	\
-			popl	%edx;			\
-			popl	%ecx;			\
-			/*popl	%eax;*/			\
-			ret;
+E(func)                 /*pushl %eax;*/ \
+                        pushl   %ecx; \
+                        pushl   %edx; \
+                        pushl   %edi; \
+                        call    SN(c_##func); \
+                        /*movb  %al,12(%esp);*/ \
+                        popl    %edx; /* dummy */ \
+                        popl    %edx; \
+                        popl    %ecx; \
+                        /*popl  %eax;*/ \
+                        ret;
 
