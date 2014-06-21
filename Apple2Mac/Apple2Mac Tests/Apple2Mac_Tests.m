ac55dee266052dc36fb9e80b3c080ba7cf234467
Aaron Culliney <devnull@deadc0de.org>
2014-06-21 12:14:15 -0700

    Adds unit test target to project, moves existing tests to that target

---
+++ Apple2Mac/Apple2Mac Tests/Apple2Mac_Tests.m
@@ -0,0 +1,34 @@
+//
+//  Apple2Mac_Tests.m
+//  Apple2Mac Tests
+//
+//  Created by Aaron Culliney on 6/21/14.
+//  Copyright (c) 2014 fl0wstate. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Apple2Mac_Tests : XCTestCase
+
+@end
+
+@implementation Apple2Mac_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
