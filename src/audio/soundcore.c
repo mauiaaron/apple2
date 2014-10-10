0b9515f2cd5b86e42d26e7bc4f4e81565e2ae0c0
Aaron Culliney <asc_at_bitr0t_com>
2014-10-09 21:17:54 -0700

    Excise ALSA, only use OpenAL

--- src/audio/soundcore.c
+++ src/audio/soundcore.c
@@ -31,10 +31,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #ifdef APPLE2IX
 
-#ifdef USE_ALSA
-#include "audio/soundcore-alsa.h"
-#endif
-
 #include "audio/win-shim.h"
 
 #else
