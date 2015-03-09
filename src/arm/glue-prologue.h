8d6961c0cb7cfbd90479060701d4af6c7a349416
Aaron Culliney <devnull@deadc0de.org>
2015-03-09 12:32:40 -0700

    Callee is responsible for saving higher regs

--- src/arm/glue-prologue.h
+++ src/arm/glue-prologue.h
@@ -18,10 +18,10 @@ ENTRY(func)             ldr     r1, SYM(softswitches); \
                         ldr     r1, SYM(pointer); \
                         tst     r0, $SS_CXROM; \
                         bne     1f; \
-                        push    {r0, EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, lr}; \
+                        push    {r0, EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ lr}; \
                         ldr     r1, [r1]; \
                         blx     r1; \
-                        pop     {r0, EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, pc}; \
+                        pop     {r0, EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ pc}; \
 1:                      ldr     r1, [r1]; \
                         ldrb    r0, [r1, EffectiveAddr]; \
                         mov     pc, lr;
@@ -48,18 +48,18 @@ ENTRY(func)             ldr     r1, SYM(pointer); \
 
 
 #define GLUE_C_WRITE(func) \
-ENTRY(func)             push    {r0, EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, lr}; \
+ENTRY(func)             push    {r0, EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ lr}; \
                         and     r0, r0, #0xff; \
                         mov     r1, r0; \
                         mov     r0, EffectiveAddr; \
                         bl      CALL(c_##func); \
-                        pop     {r0, EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, pc};
+                        pop     {r0, EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ pc};
 
 #define GLUE_C_READ(func) \
-ENTRY(func)             push    {EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, lr}; \
+ENTRY(func)             push    {EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ lr}; \
                         mov     r0, EffectiveAddr; \
                         bl      CALL(c_##func); \
-                        pop     {EffectiveAddr, PC_Reg, SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg, pc};
+                        pop     {EffectiveAddr, PC_Reg, /*SP_Reg, F_Reg, Y_Reg, X_Reg, A_Reg,*/ pc};
 
 #define GLUE_C_READ_ALTZP(FUNC) GLUE_C_READ(FUNC)
 
