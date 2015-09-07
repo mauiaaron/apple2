ef7472b3ac8b6f79b20c9aed392f0132312e68fe
Aaron Culliney <devnull@deadc0de.org>
2015-09-07 11:05:22 -0700

    Fix invalid assert ... system_buffer_size can be modified lower

--- src/audio/speaker.c
+++ src/audio/speaker.c
@@ -231,7 +231,9 @@ static void _submit_samples_buffer_fullspeed(void) {
     if (speakerBuffer->Lock(speakerBuffer, num_samples_pad*sizeof(int16_t), &system_samples_buffer, &system_buffer_size)) {
         return;
     }
-    assert(num_samples_pad <= system_buffer_size);
+    if (num_samples_pad > system_buffer_size/sizeof(int16_t)) {
+        num_samples_pad = system_buffer_size/sizeof(int16_t);
+    }
 
     //SPEAKER_LOG("bytes_queued:%d enqueueing %d quiet samples", bytes_queued, num_samples_pad);
     for (unsigned int i=0; i<num_samples_pad; i++) {
