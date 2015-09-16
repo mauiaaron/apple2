5692c1dc95bd1240c7c076fff561b3e875888795
Aaron Culliney <devnull@deadc0de.org>
2015-09-15 22:18:43 -0700

    Submit a number of quiet samples on first initialization
        This seems to make it a little less choppy on mobile first launch

--- src/audio/speaker.c
+++ src/audio/speaker.c
@@ -359,11 +359,12 @@ void speaker_init(void) {
 
         remainder_buffer_size_max = ((CLK_6502_INT*(unsigned long)CPU_SCALE_FASTEST)/audio_backend->systemSettings.sampleRateHz)+1;
 
-        samples_buffer = malloc(channelsSampleRateHz * sizeof(int16_t));
+        samples_buffer = calloc(1, channelsSampleRateHz * sizeof(int16_t));
         if (!samples_buffer) {
             err = -1;
             break;
         }
+        samples_buffer_idx = bufferSizeIdealMax;
 
         remainder_buffer = malloc(remainder_buffer_size_max * sizeof(int16_t));
         if (!remainder_buffer) {
