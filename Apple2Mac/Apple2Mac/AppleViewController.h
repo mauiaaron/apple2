bf341731f0b50109843e7dfb6c3b8ad4819432a6
jvernet <jeromevernet@hotmail.com>
2015-12-24 18:36:41 +0100

    Separate View/controller

---
+++ Apple2Mac/Apple2Mac/AppleViewController.h
@@ -0,0 +1,15 @@
+//
+//  AppleViewController.h
+//  Apple2Mac
+//
+//  Created by Jerome Vernet on 24/12/2015.
+//  Copyright © 2015 deadc0de.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppleViewController : UIViewController
+-(IBAction)rebootItem:(id)sender;
+-(IBAction)prefsItem:(id)sender;
+
+@end
---
+++ Apple2Mac/Apple2Mac/AppleViewController.m
@@ -0,0 +1,57 @@
+//
+//  AppleViewController.m
+//  Apple2Mac
+//
+//  Created by Jerome Vernet on 24/12/2015.
+//  Copyright © 2015 deadc0de.org. All rights reserved.
+//
+
+#import "AppleViewController.h"
+#import "common.h"
+#import "modelUtil.h"
+
+@interface AppleViewController ()
+
+@end
+
+@implementation AppleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+-(IBAction)rebootItem:(id)sender{
+    cpu65_reboot();
+}
+
+-(IBAction)prefsItem:(id)sender{
+    //pause
+    //show pref windows
+}
+
+- (IBAction)toggleCPUSpeedItem:(id)sender
+{
+    cpu_pause();
+    timing_toggleCPUSpeed();
+    if (video_backend && video_backend->animation_showCPUSpeed)
+    {
+        video_backend->animation_showCPUSpeed();
+    }
+    cpu_resume();
+}
+@end
