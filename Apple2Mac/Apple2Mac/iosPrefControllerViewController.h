142d6afbab243e5042a853f9babf4a77a54e2a26
jvernet <jeromevernet@hotmail.com>
2015-12-30 14:45:34 +0100

    IOS Prefs

--- Apple2Mac/Apple2Mac/iosPrefControllerViewController.h
+++ Apple2Mac/Apple2Mac/iosPrefControllerViewController.h
@@ -8,13 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
+#define kApple2SavedPrefs @"kApple2SavedPrefs"
+#define kApple2CPUSpeed @"kApple2CPUSpeed"
+#define kApple2CPUSpeedIsMax @"kApple2CPUSpeedIsMax"
+#define kApple2AltSpeed @"kApple2AltSpeed"
+#define kApple2AltSpeedIsMax @"kApple2AltSpeedIsMax"
+#define kApple2SoundcardConfig @"kApple2SoundcardConfig"
+#define kApple2ColorConfig @"kApple2ColorConfig"
+#define kApple2JoystickConfig @"kApple2JoystickConfig"
+#define kApple2JoystickAutoRecenter @"kApple2JoystickAutoRecenter"
+#define kApple2JoystickClipToRadius @"kApple2JoystickClipToRadius"
+#define kApple2PrefStartupDiskA @"kApple2PrefStartupDiskA"
+#define kApple2PrefStartupDiskAProtected @"kApple2PrefStartupDiskAProtected"
+#define kApple2PrefStartupDiskB @"kApple2PrefStartupDiskB"
+#define kApple2PrefStartupDiskBProtected @"kApple2PrefStartupDiskBProtected"
+#define kApple2JoystickStep @"kApple2JoystickStep"
+
+
 @interface iosPrefControllerViewController : UIViewController<UIPickerViewDataSource, UIPickerViewDelegate>
-{
-    NSArray *_pickerData;
-}
-@property (weak, nonatomic) IBOutlet UIPickerView *videoModePicker;
-@property (retain, nonatomic) IBOutlet UISlider *CPUSpeed;
-@property (retain, nonatomic) IBOutlet UISwitch *CPUMax;
+
+@property (retain, nonatomic) IBOutlet UIPickerView *videoModePicker;
+@property (retain, nonatomic) IBOutlet UISlider *cpuSlider;
+@property (retain, nonatomic) IBOutlet UISlider *altSlider;
+@property (retain, nonatomic) IBOutlet UILabel *cpuSliderLabel;
+@property (retain, nonatomic) IBOutlet UILabel *altSliderLabel;
+@property (retain, nonatomic) IBOutlet UISwitch *cpuMaxChoice;
+@property (retain, nonatomic) IBOutlet UISwitch *altMaxChoice;
+
+@property (strong,nonatomic) NSArray *_videoMode;
 
 
 @end
--- Apple2Mac/Apple2Mac/iosPrefControllerViewController.m
+++ Apple2Mac/Apple2Mac/iosPrefControllerViewController.m
@@ -18,11 +18,94 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    _pickerData = @[@"Mono", @"Color"];
+    self._videoMode  = [[NSArray alloc]  initWithObjects:@"Mono",@"Color", nil];
     
     // Connect data
     self.videoModePicker.dataSource = self;
     self.videoModePicker.delegate = self;
+
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    BOOL firstTime = ![defaults boolForKey:kApple2SavedPrefs];
+    if (firstTime)
+    {
+        [defaults setBool:YES forKey:kApple2SavedPrefs];
+        [defaults setDouble:1.0 forKey:kApple2CPUSpeed];
+        [defaults setDouble:CPU_SCALE_SLOWEST forKey:kApple2AltSpeed];
+        [defaults setBool:NO forKey:kApple2CPUSpeedIsMax];
+        [defaults setBool:NO forKey:kApple2AltSpeedIsMax];
+        [defaults setInteger:COLOR_INTERP forKey:kApple2ColorConfig];
+       // [defaults setInteger:JOY_KPAD forKey:kApple2JoystickConfig];
+        [defaults setBool:YES forKey:kApple2JoystickAutoRecenter];
+        [defaults removeObjectForKey:kApple2PrefStartupDiskA];
+        [defaults removeObjectForKey:kApple2PrefStartupDiskB];
+    }
+    
+    cpu_scale_factor = [defaults doubleForKey:kApple2CPUSpeed];
+    [self.cpuSlider setValue:cpu_scale_factor animated:NO];
+    self.cpuSliderLabel.text=[NSString stringWithFormat:@"%.0f%%", cpu_scale_factor*100];
+    if ([defaults boolForKey:kApple2CPUSpeedIsMax])
+    {
+        cpu_scale_factor = CPU_SCALE_FASTEST;
+        [self.cpuMaxChoice setOn:YES];
+        [self.cpuSlider setEnabled:NO];
+    }
+    else
+    {
+        [self.cpuMaxChoice setOn:NO];
+        [self.cpuSlider setEnabled:YES];
+    }
+    
+    cpu_altscale_factor = [defaults doubleForKey:kApple2AltSpeed];
+    [self.altSlider setValue:cpu_altscale_factor];
+    self.altSliderLabel.text = [NSString stringWithFormat:@"%.0f%%", cpu_altscale_factor*100];
+    if ([defaults boolForKey:kApple2AltSpeedIsMax])
+    {
+        cpu_altscale_factor = CPU_SCALE_FASTEST;
+        [self.altMaxChoice setOn:YES];
+        [self.altSlider setEnabled:NO];
+    }
+    else
+    {
+        [self.altMaxChoice setOn:NO];
+        [self.altSlider setEnabled:YES];
+    }
+
+    NSInteger mode = [defaults integerForKey:kApple2ColorConfig];
+    if (! ((mode >= COLOR_NONE) && (mode < NUM_COLOROPTS)) )
+    {
+        mode = COLOR_NONE;
+    }
+    //[self.videoModePicker d:mode];
+    color_mode = (color_mode_t)mode;
+
+    mode = [defaults integerForKey:kApple2JoystickConfig];
+    if (! ((mode >= JOY_PCJOY) && (mode < NUM_JOYOPTS)) )
+    {
+        mode = JOY_PCJOY;
+    }
+    joy_mode = (joystick_mode_t)mode;
+//    [self.joystickChoice selectItemAtIndex:mode];
+/*
+#ifdef KEYPAD_JOYSTICK
+    joy_auto_recenter = [defaults integerForKey:kApple2JoystickAutoRecenter];
+    [self.joystickRecenter setState:joy_auto_recenter ? NSOnState : NSOffState];
+    joy_step = [defaults integerForKey:kApple2JoystickStep];
+    if (!joy_step)
+    {
+        joy_step = 1;
+    }
+    [self.joystickStepLabel setIntegerValue:joy_step];
+    [self.joystickStepper setIntegerValue:joy_step];
+#endif
+    
+    joy_clip_to_radius = [defaults boolForKey:kApple2JoystickClipToRadius];
+    [self.joystickClipToRadius setState:joy_clip_to_radius ? NSOnState : NSOffState];
+    
+    [self _setupJoystickUI];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(drawJoystickCalibration:) name:(NSString *)kDrawTimerNotification object:nil];
+*/
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,13 +122,14 @@
 // The number of rows of data
 - (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    return _pickerData.count;
+    return 2;
+    //_pickerData.count;
 }
 
 // The data to return for the row and component (column) that's being passed in
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
-    return _pickerData[row];
+     return [self._videoMode objectAtIndex:row];
 }
 
 // Catpure the picker view selection
@@ -53,6 +137,7 @@
 {
     // This method is triggered whenever the user makes a change to the picker selection.
     // The parameter named row and component represents what was selected.
+    NSLog(@"Selected Row %d", row);
 }
 
 /*
@@ -66,10 +151,14 @@
  */
 
 - (void)dealloc {
-    [_videoModePicker release];
-    [_videoModePicker release];
-    [_CPUSpeed release];
-    [_CPUMax release];
+  /*  [_videoModePicker release];
+    [_cpuSlider release];
+    [_altSlider release];
+    [_cpuSliderLabel release];
+    [_altSlider release];
+    [_cpuMaxChoice release];
+    [_altMaxChoice release];
+   */
     [super dealloc];
 }
 
